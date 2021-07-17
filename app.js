7d0aeac8afd987b15168bcca11989dff79084eca
Ayberk Arıcı <ayberk816gg@gmail.com>
2021-07-17 18:05:16 +0300

    Details

--- app.js
+++ app.js
@@ -75,6 +75,8 @@ $(document).ready(function () {
                         set_depth(inputValue);
                     } else if (elem.dataset.lengthtype == "width") {
                         set_width(inputValue);
+                    } else if (elem.dataset.lengthtype == "height") {
+                        write_length_height('.area_length_value_height_variable', inputValue);
                     }
                 }
             }
@@ -82,7 +84,7 @@ $(document).ready(function () {
     }
 
     function set_depth(value) {
-        if (value >= 40 && value <= 350) {
+        if (value >= 50 && value <= 350) {
             let leftPos = value - 161;       
             
             // * A * 
@@ -97,6 +99,9 @@ $(document).ready(function () {
             }); 
 
             $('.left')[0].style.transform = "rotateY(90deg) translate3d(51px, -8px," + leftPos + "px)";
+
+            write_length('.area_length_value_depth', value);
+
             
 
         } else if (value >= 350) {
@@ -108,18 +113,24 @@ $(document).ready(function () {
                 
                 "width": "350px"
             });
+
             $('.left')[0].style.transform = "rotateY(90deg) translate3d(51px, -8px, 189px)";
+
+            write_length('.area_length_value_depth', value);
         
         } else {
             $('.top').css({
                 
-                "width" : "220px"
+                "width" : "50px"
             });
             $('.front').css({
                 
-                "width": "220px"
+                "width": "50"
             });
-            $('.left')[0].style.transform = "rotateY(90deg) translate3d(51px, -8px, 59px)";
+
+            $('.left')[0].style.transform = "rotateY(90deg) translate3d(51px, -8px, -111px)";
+
+            write_length('.area_length_value_depth', value);
         }
     }
     
@@ -134,19 +145,35 @@ $(document).ready(function () {
 
             $('.left').css("height", value + "px");
 
+            write_length('.area_length_value_width', value);
+
         } else if (value >= 120) {
             $('.top').css({"height" : 120 + "px"});
             
             $('.front')[0].style.transform = "rotateX(90deg) translateX(-161px)  translateY(-40px) translateZ(-" + 88 +"px)";
 
             $('.left').css("height", 120 + "px");
+
+            write_length('.area_length_value_width', value);
         } else {
             $('.top').css({"height" : 76 + "px"});
             
             $('.front')[0].style.transform = "rotateX(90deg) translateX(-161px)  translateY(-40px) translateZ(-" + 44 +"px)";
 
             $('.left').css("height", 76 + "px");
+
+            write_length('.area_length_value_width', value);
         }
+    }
+
+    function write_length(classname, value) {
+        $(classname).html("←" + value + "cm→");
+    }
+
+    function write_length_height(classname, value) {
+        if (value > 2 && value <= 17) {
+            $(classname).html(value);
+        } 
     }
 })
 
