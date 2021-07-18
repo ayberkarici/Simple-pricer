0c16d3745108f5d47f763c35f42be77da8461e37
Ayberk Arıcı <ayberk816gg@gmail.com>
2021-07-18 22:27:51 +0300

    3d moving process has started

--- app.js
+++ app.js
@@ -23,8 +23,9 @@ $(document).ready(function () {
             }
 
             currentLengthType = "inch";
-
             cminch = false;
+
+
         } else {
             // Selected CM
             $('.cm').removeClass("unchoosedType");
@@ -43,17 +44,14 @@ $(document).ready(function () {
                 if(!isNaN(inputValue)) {
                     let result = Math.round((inputValue  * (2.54) + Number.EPSILON) * 100) / 100;
                     $('.dimension')[i].value = result;
-
                 }
             }
 
             currentLengthType = "cm";
             cminch = true;
-
+            set_table(currentLengthType);
 
         }
-
-
     })
 
     $('.dimension').on('keyup', function () {
@@ -65,27 +63,65 @@ $(document).ready(function () {
     })
 
     function set_table(type) {
-        if (type == "cm") {
-            for (let i = 0; i < $('.dimension').length; i++) {
-                let elem = $('.dimension')[i];
-                let inputValue = Number(elem.value);
+        
+        for (let i = 0; i < $('.dimension').length; i++) {
+            let elem = $('.dimension')[i];
+            let inputValue = Number(elem.value);
+
+            if(type == "cm") {
+                if (!isNaN(inputValue)) {
+                    if (elem.dataset.lengthtype == "depth") {
+                        $('.dimension')[i].attributes.min.value = 40;
+                        $('.dimension')[i].attributes.max.value = 400;
+                        $('.dimension')[i].attributes.step.value = 5;
+
+                        set_depth(inputValue);
+                    } else if (elem.dataset.lengthtype == "width") {
+                        $('.dimension')[i].attributes.min.value = 66;
+                        $('.dimension')[i].attributes.max.value = 130;
+                        $('.dimension')[i].attributes.step.value = 2;
+
+                        set_width(inputValue);
+                    } else if (elem.dataset.lengthtype == "height") {
+                        $('.dimension')[i].attributes.min.value = 5;
+                        $('.dimension')[i].attributes.max.value = 17;
+                        $('.dimension')[i].attributes.step.value = 1;
+
+                        write_length_height('.area_length_value_height_variable', inputValue);
+                    }
+                }
+            } else  if (type == "inch") {
+                let resultInch = Math.round((inputValue  * (2.54) + Number.EPSILON) * 100) / 100;
 
                 if (!isNaN(inputValue)) {
                     if (elem.dataset.lengthtype == "depth") {
-                        set_depth(inputValue);
+                        $('.dimension')[i].attributes.min.value = 15.74;
+                        $('.dimension')[i].attributes.max.value = 157.48;
+                        $('.dimension')[i].attributes.step.value = 3;
+
+                        set_depth(resultInch);
                     } else if (elem.dataset.lengthtype == "width") {
-                        set_width(inputValue);
+                        $('.dimension')[i].attributes.min.value = 25.98;
+                        $('.dimension')[i].attributes.max.value = 51.18;
+                        $('.dimension')[i].attributes.step.value = 2;
+
+                        set_width(resultInch);
                     } else if (elem.dataset.lengthtype == "height") {
-                        write_length_height('.area_length_value_height_variable', inputValue);
+                        $('.dimension')[i].attributes.min.value = 1.96;
+                        $('.dimension')[i].attributes.max.value = 6.69;
+                        $('.dimension')[i].attributes.step.value = 0.80;
+
+                        write_length_height('.area_length_value_height_variable', resultInch);
                     }
                 }
             }
         }
+        
     }
 
     function set_depth(value) {
         if (value >= 50 && value <= 350) {
-            let leftPos = value - 161;       
+            let leftPos = value - 158;
             
             // * A * 
             $('.top').css({
@@ -98,7 +134,7 @@ $(document).ready(function () {
                 "width" : value + "px"
             }); 
 
-            $('.left')[0].style.transform = "rotateY(90deg) translate3d(51px, -8px," + leftPos + "px)";
+            $('.left')[0].style.transform = "rotateY(90deg) translate3d(56px, 0px," + leftPos + "px)";
 
             write_length('.area_length_value_depth', value);
 
@@ -114,7 +150,7 @@ $(document).ready(function () {
                 "width": "350px"
             });
 
-            $('.left')[0].style.transform = "rotateY(90deg) translate3d(51px, -8px, 189px)";
+            $('.left')[0].style.transform = "rotateY(90deg) translate3d(56px, 0px, 192px)";
 
             write_length('.area_length_value_depth', value);
         
@@ -128,7 +164,7 @@ $(document).ready(function () {
                 "width": "50"
             });
 
-            $('.left')[0].style.transform = "rotateY(90deg) translate3d(51px, -8px, -111px)";
+            $('.left')[0].style.transform = "rotateY(90deg) translate3d(56px, 0px, -108px)";
 
             write_length('.area_length_value_depth', value);
         }
@@ -136,12 +172,12 @@ $(document).ready(function () {
     
     function set_width(value) {
         if (value >= 76 && value <= 120) {
-            let frontPos = value - 32;
+            let frontPos = value - 8;
 
             // * B * 
             $('.top').css("height" , value + "px");
             
-            $('.front')[0].style.transform = "rotateX(90deg) translateX(-161px)  translateY(-40px) translateZ(-" + frontPos +"px)";
+            $('.front')[0].style.transform = "rotateX(90deg) translateX(-150px)  translateY(-56px) translateZ(-" + frontPos +"px)";
 
             $('.left').css("height", value + "px");
 
@@ -150,7 +186,7 @@ $(document).ready(function () {
         } else if (value >= 120) {
             $('.top').css({"height" : 120 + "px"});
             
-            $('.front')[0].style.transform = "rotateX(90deg) translateX(-161px)  translateY(-40px) translateZ(-" + 88 +"px)";
+            $('.front')[0].style.transform = "rotateX(90deg) translateX(-150px)  translateY(-56px) translateZ(-" + 112 +"px)";
 
             $('.left').css("height", 120 + "px");
 
@@ -158,7 +194,7 @@ $(document).ready(function () {
         } else {
             $('.top').css({"height" : 76 + "px"});
             
-            $('.front')[0].style.transform = "rotateX(90deg) translateX(-161px)  translateY(-40px) translateZ(-" + 44 +"px)";
+            $('.front')[0].style.transform = "rotateX(90deg) translateX(-150px)  translateY(-56px) translateZ(-" + 68 +"px)";
 
             $('.left').css("height", 76 + "px");
 
@@ -171,9 +207,54 @@ $(document).ready(function () {
     }
 
     function write_length_height(classname, value) {
-        if (value > 2 && value <= 17) {
+        if (value > 4 && value <= 17) {
             $(classname).html(value);
         } 
     }
+    
+    dragElement($('.prototype'));
+
+    function dragElement(elmnt) {
+      let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+      if (elmnt) {
+        /* if present, the header is where you move the DIV from:*/
+        elmnt.onmousedown = dragMouseDown;
+      } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        elmnt.onmousedown = dragMouseDown;
+      }
+    
+      function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+      }
+    
+      function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        $('.cube')[0].style.transform  = "rotateX(" + pos4 + "deg) rotateY(0deg) rotateZ(-150deg) translateZ(35px)";
+        console.log($('.cube')[0].style.transform );
+      }
+    
+      function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+      }
+    }
 })
+
 
